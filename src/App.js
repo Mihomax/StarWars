8c25e441f51bebc735a479a318f246f6529682ba
Hovhannes Hambardzumyan <hovhannes.hambardzumyan@yahoo.com>
2018-08-09 11:11:35 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,68 @@
 import React,{Component} from 'react';
 import  "./css/mycss.css";
 import axios from 'axios';
-import {BrowserRouter as Router} from 'react-router-dom';
-//import Route from 'react-router-dom/Route';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
 //import Content from './components/Content/Content';
+
 
 class App extends Component {
 	constructor(props) {
     super(props);
 
 this.state = {
+	server: 'http://127.0.0.1/api.php',
 	data:[],
+	peopleData: [],
+	planetsData: [],
+	starshipsData: [],
+
 	people:'people',
 	planets:'planets',
 	starships:'starships',
 	search:'',
-	current:''
+	current:'people'
 }
 this.handleChange = this.handleChange.bind(this);
-this.postCommand = this.postCommand.bind(this);
+this.dataChanger = this.dataChanger.bind(this);
+this.searchQuery = this.searchQuery.bind(this);
 	};
 
-	postCommand = (command) => {
-		//this.setState({current:command});
-		axios.post('http://127.0.0.1/api.php', command, {
+	componentWillMount = () => {
+		
+		axios.post(this.state.server, this.state.people, {
 			headers : {
 					'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
 			}
 	})
 		.then((res) => {
-			//console.log(res.data.results);
-			this.setState({data:res.data.results});
+			this.setState({dataPeople:res.data.results});
+			//console.log(this.state.dataPeople);
+		})
+		.catch((err) => {
+			console.log("AXIOS ERROR: ", err);
+		});
+		
+		axios.post(this.state.server, this.state.planets, {
+			headers : {
+					'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+			}
+	})
+		.then((res) => {
+			this.setState({dataPlanets:res.data.results});
+			//console.log(this.state.dataPlanets);
+		})
+		.catch((err) => {
+			console.log("AXIOS ERROR: ", err);
+		});
+
+		axios.post(this.state.server, this.state.starships, {
+			headers : {
+					'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+			}
+	})
+		.then((res) => {
+			this.setState({dataStarships:res.data.results});
+			//console.log(this.state.dataStarships);
 		})
 		.catch((err) => {
 			console.log("AXIOS ERROR: ", err);
@@ -38,78 +70,53 @@ this.postCommand = this.postCommand.bind(this);
 
 	}
 
-	handleChange = (event) => {
-this.setState({search:event.target.value});
+	dataChanger = (command) => {
+		this.setState({data:command});
+	}
+	searchQuery = (command) => {
+		axios.post(this.state.server, command, {
+			headers : {
+					'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+			}
+	})
+		.then((res) => {
+			//console.log(res.data.results);
+			this.setState({data:res.data.results});
+			
+		})
+		.catch((err) => {
+			console.log("AXIOS ERROR: ", err);
+		});
+	}
+
+handleChange = (event) => {
+	this.setState({search:event.target.value});
 
 	}
 
-    render() {
-		const style = {color:"white"};
-		
-		let result = this.state.data.map((each,index) => <li key={index} style={style}> {each.name}</li>
+render() {
+	const style = {color:"white"};
+	let result = this.state.data.map((each,index) => <li key={index} style={style}> {each.name}</li>
 		);
 			
-        return (
-			<Router>
-              <div>
-				  
-				  <button className="w3-btn" onClick={(e) => this.postCommand(this.state.people)}>Click to see SW guys</button>
-				  <button className="w3-btn" onClick={(e) => this.postCommand(this.state.planets)}>Click to see all the planets</button>
-				  <button className="w3-btn" onClick={(e) => this.postCommand(this.state.starships)}>Click to see starships</button><br/>
-				  <input  onChange={this.handleChange} value={this.state.search} placeholder="Person's name here ..."/><br/>
-				  <button className="w3-btn" onClick={(e) => this.postCommand("people/?search="+this.state.search)}>Search for persons</button>
-				  <p>May the force be with you . . .</p>
-				  {/* <Content content = {result} option = {this.state.current}/> */}
-				  {/* <Route path="/people" render = {() => <ul> {result} </ul> }   /> */}
-				  <ul> {result} </ul>
-              </div>    
-			  </Router>  
-        )
+    return (
+		<Router>
+            <div>
+				<Link  className="w3-btn" onClick={(e) => this.dataChanger(this.state.dataPeople)} to="/people">Click to see SW guys</Link>
+				<Link  className="w3-btn" onClick={(e) => this.dataChanger(this.state.dataPlanets)} to="/planets">Click to see all planets</Link>
+				<Link  className="w3-btn" onClick={(e) => this.dataChanger(this.state.dataStarships)} to="/starships">Click to see starships</Link><br/>
+				<input  onChange={this.handleChange} value={this.state.search} placeholder="Person's name here ..."/><br/>
+				<Link  className="w3-btn" onClick={(e) => this.searchQuery("/?search="+this.state.search)} to="/search">Search for persons</Link>
+
+				<p>May the force be with you . . .</p>
+				{/* <Content content = {result} option = {this.state.current}/> */}
+		 			<ul> {result} </ul> 
+				 
+			</div>    
+		</Router>
+			  )
 }
 }
-  
-
-
 
 export default App;
 
-// componentWillMount () {
-		
-// axios.post('http://127.0.0.1/php_test/api.php', this.state.people, {
-// 			headers : {
-// 					'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
-// 			}
-// 	})
-// 		.then((res) => {
-// 			console.log(res.data.results);
-// 			this.setState({data:res.data.results});
-// 		})
-// 		.catch((err) => {
-// 			console.log("AXIOS ERROR: ", err);
-// 		})
-
-// 	}
-
-// componentWillMount () {
-  //   fetch('http://127.0.0.1/php_test/api.php')
-  //     .then(response => response.json())
-  //     .then((responseJSON) =>  this.setState({data:responseJSON.data}))
-	// 	};
-		
-		// componentWillMount () {
-		// 	const formData = new FormData();
- 		// 	formData.append('name', "Sam")
-		// 	axios.post('http://127.0.0.1/php_test/api.php', formData)
-		// 	.then((response) => {
-		// 		this.setState({data:response.data});
-		// 	});
-		// };
-
-		// 	componentWillMount () {
-// 	fetch('http://127.0.0.1/php_test/api.php', {
-//   method: 'POST',
-//   headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-//   body: JSON.stringify({text: "blablabla", id_product: "12"})
-// }).then((response) => response.text())
-// .then((responseData) => { console.log("response: " + responseData); })
-// .catch((err) => { console.log(err); });
---
+++ src/draft.js
@@ -0,0 +1,54 @@
+	// 	axios.post('http://127.0.0.1/api.php', command, {
+	// 		headers : {
+	// 				'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+	// 		}
+	// })
+	// 	.then((res) => {
+	// 		//console.log(res.data.results);
+	// 		this.setState({data:res.data.results});
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log("AXIOS ERROR: ", err);
+    // 	});
+    
+
+    // componentWillMount () {
+		
+// axios.post('http://127.0.0.1/php_test/api.php', this.state.people, {
+// 			headers : {
+// 					'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+// 			}
+// 	})
+// 		.then((res) => {
+// 			console.log(res.data.results);
+// 			this.setState({data:res.data.results});
+// 		})
+// 		.catch((err) => {
+// 			console.log("AXIOS ERROR: ", err);
+// 		})
+
+// 	}
+
+// componentWillMount () {
+  //   fetch('http://127.0.0.1/php_test/api.php')
+  //     .then(response => response.json())
+  //     .then((responseJSON) =>  this.setState({data:responseJSON.data}))
+	// 	};
+		
+		// componentWillMount () {
+		// 	const formData = new FormData();
+ 		// 	formData.append('name', "Sam")
+		// 	axios.post('http://127.0.0.1/php_test/api.php', formData)
+		// 	.then((response) => {
+		// 		this.setState({data:response.data});
+		// 	});
+		// };
+
+		// 	componentWillMount () {
+// 	fetch('http://127.0.0.1/php_test/api.php', {
+//   method: 'POST',
+//   headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+//   body: JSON.stringify({text: "blablabla", id_product: "12"})
+// }).then((response) => response.text())
+// .then((responseData) => { console.log("response: " + responseData); })
+// .catch((err) => { console.log(err); });
